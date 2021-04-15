182b099ae844f54535fa564a6d3491d56408ada5
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-04-15 20:48:41 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBsV0jGhMGiqaKuQciAWmUYvf02VazdwQY",
+    authDomain: "clone-e9425.firebaseapp.com",
+    projectId: "clone-e9425",
+    storageBucket: "clone-e9425.appspot.com",
+    messagingSenderId: "522972319238",
+    appId: "1:522972319238:web:542f4cb4d202dc78d57d87",
+    measurementId: "G-EDLJFS5FKJ"
+  };
