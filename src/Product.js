0ea8137396653f5e69903c6144990ab61ff53932
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-04-18 15:23:44 +0900

    dispatched action

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
-function Product({ title, image, price, rating}) {
+function Product({ id,title, image, price, rating}) {
+    const [{basket} , dispatch] = useStateValue();
+
+    console.log('this is the basket>>>> ', basket);
+    const addToBasket = () => {
+        dispatch({
+            type : 'ADD_TO_BASKET',
+            item : {
+                id : id,
+                title : title,
+                image : image,
+                price : price,
+                rating : rating,
+            }
+        })
+    }
+
     return (
         <div className="product">
             <div className="product__info">
@@ -20,10 +37,11 @@ function Product({ title, image, price, rating}) {
                 </div>
             </div>
             <img src={image} alt=""/>
-            <button>Add to Basket</button>
+            <button
+                onClick={addToBasket}>Add to Basket</button>
         </div>
 
     )
 }
 
-export default Product
+export default Product;
