bebfa89e215ac62e6e5dca8f802d6f5a2f22dd1d
Matt Kim (김명준) <matt85kim53@gmail.com>
2021-04-18 12:49:56 +0900

    product & router added

--- src/Header.js
+++ src/Header.js
@@ -20,7 +20,7 @@ function Header() {
                     <span className='header__optionLineTwo'>Sign In</span> 
                  </div>
                  <div className="header__option">
-                    <span className='header__optionLineOne'>Returnst</span> 
+                    <span className='header__optionLineOne'>Returns</span> 
                     <span className='header__optionLineTwo'>& Orders</span> 
                  </div>
                  <div className="header__option">
